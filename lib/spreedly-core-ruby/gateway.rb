32fdd30dae0247d0dc5a3c70390beff29858240d
Joey Beninghove <joeybeninghove@gmail.com>
2012-10-08 21:29:03 -0400

    Adding attribute for supports_offsite_purchase to the gateway class

--- lib/spreedly-core-ruby/gateway.rb
+++ lib/spreedly-core-ruby/gateway.rb
@@ -2,7 +2,7 @@ module SpreedlyCore
   class Gateway < Base
     attr_reader(:name, :token, :gateway_type, :auth_modes, :supports_capture,
                 :supports_authorize, :supports_purchase, :supports_void,
-                :supports_credit, :redacted)
+                :supports_credit, :supports_offsite_purchase, :redacted)
 
     # returns an array of Gateway which are supported
     def self.supported_gateways
